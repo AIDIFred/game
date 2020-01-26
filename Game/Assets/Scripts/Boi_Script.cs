213c3394f19be15ef7201f230f81b508aa53d17b
Dan <danevans264@gmail.com>
2020-01-26 02:26:38 -0600

    movements

---
+++ Game/Assets/Scripts/Boi_Script.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boi_Script : MonoBehaviour {
+    int frame = 0;
+    public Rigidbody2D body;
+    bool A_down = false;
+    bool D_down = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        check_keys();
+        Debug.Log(A_down);
+        if (A_down == true) {
+            body.AddForce(new Vector2(-10, 0));
+        }
+
+
+        
+
+
+
+        frame++;
+	}
+
+    void check_keys() {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            A_down = true;
+        }
+        if (Input.GetKeyUp(KeyCode.A)) {
+            A_down = false;
+        }
+    }
+}
